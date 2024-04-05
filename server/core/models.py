f293b19310b69b749be7907bc79e1a2c5a938e77
Harshul-25 <charshul25@gmail.com>
2024-04-06 05:21:22 +0530

    created some models

--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+ROLES = [('user','normal user'),('expert','domain expert')]
+
 # Create your models here.
+class user(models.Model):
+    email = models.CharField(max_length=50,required=True)
+    display_name = models.CharField(max_length=100, unique=True, required=True)
+    role = models.CharField(choices=ROLES, max_length=50,required=True)
+    profession = models.CharField(max_length=50,null=True,default=None)
+    def __str__(self):
+        return str(self.display_name)
+
+class location(models.Model):
+    username = models.ForeignKey(user,max_length=50)
+    country = models.CharField(max_length=100,required=True)
+    state = models.CharField(max_length=100,required=True)
+    district = models.CharField(max_length=100,required=True)
+    
