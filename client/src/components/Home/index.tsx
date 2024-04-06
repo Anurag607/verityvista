9e5f6046c09a8e24e97196ca38a1f409b16ab1e1
Harshul <nawaniharshul2@gmail.com>
2024-04-06 07:19:19 +0530

    added loader and made some ui changes

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -57,20 +57,20 @@ const Dashboard = () => {
                 <FileTextIcon className="h-4 w-4" />
                 Articles
               </Link>
-              <Link
+              {/* <Link
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                 href="#"
               >
                 <UsersIcon className="h-4 w-4" />
                 Users
-              </Link>
-              <Link
+              </Link> */}
+              {/* <Link
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                 href="#"
               >
                 <SettingsIcon className="h-4 w-4" />
                 Settings
-              </Link>
+              </Link> */}
             </nav>
           </div>
           <div className="p-4">
@@ -115,20 +115,20 @@ const Dashboard = () => {
               <FileTextIcon className="h-4 w-4" />
               Articles
             </Link>
-            <Link
+            {/* <Link
               className="flex items-center gap-2 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
               href="#"
             >
               <UsersIcon className="h-4 w-4" />
               Users
-            </Link>
-            <Link
+            </Link> */}
+            {/* <Link
               className="flex items-center gap-2 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
               href="#"
             >
               <SettingsIcon className="h-4 w-4" />
               Settings
-            </Link>
+            </Link> */}
           </nav>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -153,9 +153,9 @@ const Dashboard = () => {
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
+              {/* <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem> */}
               <DropdownMenuSeparator />
               <DropdownMenuItem>Logout</DropdownMenuItem>
             </DropdownMenuContent>
