9a3bf0d1866e9a3fadb8c29f49646ccb290554b7
Harshit0009 <tiwariharshit909@gmail.com>
2024-04-06 12:02:39 +0530

    completed req and res for fact check

---
+++ server/factcheck/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import FactReqModel, FactResModel
+from .serializers import FactReqModelSerializer, FactResModelSerializer
+
+class FactReqModelListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = FactReqModelSerializer
+
+
+    def get_queryset(self):
+        return FactReqModel.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FactReqModelRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = FactReqModel.objects.all()
+    serializer_class = FactReqModelSerializer
+
+
+class FactResModelListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = FactResModelSerializer
+
+
+    def get_queryset(self):
+        return FactResModel.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FactResModelRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = FactResModel.objects.all()
+    serializer_class = FactResModelSerializer
+
+@api_view(['POST'])
+def upvote(request, pk):
+    try:
+        fact_req = FactReqModel.objects.get(pk=pk)
+    except FactReqModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    fact_req.up()
+    serializer = FactReqModelSerializer(fact_req)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def downvote(request, pk):
+    try:
+        fact_req = FactReqModel.objects.get(pk=pk)
+    except FactReqModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    fact_req.down()
+    serializer = FactReqModelSerializer(fact_req)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_response(request, pk):
+    response = request.data.get('response')
+    if response not in [-1, 0, 1]:
+        return Response({'error': 'Invalid response'}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        fact_req = FactReqModel.objects.get(pk=pk)
+        fact_res = FactResModel.objects.create(foreignId=fact_req, message=request.data.get('message'), closed=False)
+        if response == -1:
+            fact_res.countNegRes += 1
+        elif response == 1:
+            fact_res.countPosRes += 1
+        else:
+            fact_res.noSentimentRes += 1
+
+        fact_res.save()
+    except FactReqModel.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    serializer = FactResModelSerializer(fact_res)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'factcheck',
     "core",
     "rest_framework",
 ]
